c196b5b30f278755187a53c57a3a32f4596e5837
Francescosommellaa <franc.sommella@gmail.com>
2024-09-15 18:47:08 +0200

    completa e consegna

--- database/seeders/TravelPackagesSeeder.php
+++ database/seeders/TravelPackagesSeeder.php
@@ -2,8 +2,9 @@
 
 namespace Database\Seeders;
 
-use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Faker\Factory as Faker;
 
 class TravelPackagesSeeder extends Seeder
 {
@@ -12,6 +13,18 @@ class TravelPackagesSeeder extends Seeder
      */
     public function run(): void
     {
-        //
+        $faker = Faker::create();
+
+        for ($i = 0; $i < 50; $i++) {
+            DB::table('travel_packages')->insert([
+                'destination' => $faker->city,
+                'description' => $faker->paragraph,
+                'start_date' => $faker->date,
+                'end_date' => $faker->date,
+                'price' => $faker->randomFloat(2, 100, 5000),
+                'created_at' => now(),
+                'updated_at' => now(),
+            ]);
+        }
     }
 }
